12ada9be51cddac462a80e10c1dedd8a80db4b8f
Kevin Yogi <kevinyogi@Kevins-MacBook-Pro.local>
2017-08-19 11:54:37 -1000

    actually finished splice exercises

--- arrayMethods.js
+++ arrayMethods.js
@@ -34,6 +34,8 @@ mixedWords.sort();
 var notFruit = fruitCollection.splice(1, 3);
 gemBox.splice(5, 2);
 gemBox.splice(6);
+upToTen.splice(2, 0, 3, 4, 5, 6, 7, 8);
+brownOnly.splice(1, 5, "brown", "brown", "brown");
 
 
 
